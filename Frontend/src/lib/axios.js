f8dd175651ff8a1d3c2f874725ef0f21fdbc8814
rishabh-prince <rs1179379@gmail.com>
2024-12-30 23:15:51 +0530

    path updated

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "production"
-      ? "https://connectify-ykb0.onrender.com"
+      ? "https://connectify-ykb0.onrender.com/api"
       : "http://localhost:5001/api",
   withCredentials: true,
 });
