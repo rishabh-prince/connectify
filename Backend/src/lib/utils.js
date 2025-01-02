4f3f1b93cc88a0bd2c4b44b47caef49826915df3
rishabh-prince <rs1179379@gmail.com>
2025-01-02 11:37:25 +0530

    error handled

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
+  try{
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
@@ -9,7 +10,10 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
-    secure: process.env.NODE_ENV === "production",
-  });
-  return token;
+    secure: process.env.NODE_ENV !== "development",
+  })
+ return token;
+}catch (error) {
+  console.error("Error generating token:", error);
+  }
 };
